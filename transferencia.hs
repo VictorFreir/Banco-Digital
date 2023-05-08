51f1b34263fdffa5a796a8ac160908f09f4f8d3b
VictorFreir <victorvfreire@gmail.com>
2023-05-08 01:09:38 -0300

    Adiciona menu da transferencia

--- transferencia.hs
+++ transferencia.hs
@@ -1,14 +1,38 @@
-import Models.People as People
+import Models.Conta as Conta
+import Data.Time.Clock (getCurrentTime, UTCTime)
 
-transferir :: People -> People -> Float -> String
-transferir contaSaida valor contaChegada = do
-    if saldo >= valor then do 
-        contaSaida <- subtraiValor contaSaida valor
-        contaChegada <- contaChegada #implementar
-    else do 
-        "erro"
+menu :: Conta -> IO ()
+menu contaAtual = do
+    putStrLn "Bem vindo a área de transferências!"
+    putStrLn "Informe o valor a ser transferido: "
+    valor <- readLine :: Float
+    if valor < (show saldo contaAtual) then do
+        let contaDestinataria = encontraContaDestinataria cpfDestinatario
+        if ((show identificador contaDestinaria) == 0) then erroCpf else transferir contaAtual contaDestinataria valor
+    else erroValor
 
-subtraiValor :: People -> Float -> People
+encontraContaDestinataria :: IO -> Conta
+encontraContaDestinataria = contaDestinaria where
+    contaDestinataria = Conta 0 "nome" "numero" pegarDiaAtual "endereco" "1234" "pergunta" "resposta" 0.0
+
+erroCpf :: IO()
+erroCpf = do
+    print "O CPF informado não corresponde a nenhuma conta."
+
+erroValor :: IO()
+erroValor = do
+    print "O valor desejado é superior ao valor disponível em sua conta!"
+
+transferir :: Conta -> Conta -> Float -> String
+transferir contaSaida contaChegada valor = do
+    let novaContaSaida = subtraiValor contaSaida valor
+        novaContaChegada = somaValor contaChegada valor
+    salvaConta novaContaSaida
+    salvaConta novaContaChegada
+    return "Transferência realizada com sucesso!"
+
+
+subtraiValor :: Conta -> Float -> Conta
 subtraiValor conta valor = do
     let identificadorConta = (show $ identificador conta)
     let nomeConta = (show $ nome conta)
@@ -20,9 +44,9 @@ subtraiValor conta valor = do
     let perguntaSecretaConta = (show $ perguntaSecreta conta)
     let respostaSecretaConta = (show $ respostaSecreta conta)
     let saldoConta = (show $ saldo conta) - valor
-    People identificadorConta nomeConta cpfConta numeroContaConta dataNascimentoConta enderecoConta senhaConta perguntaSecretaConta respostaSecretaConta saldoConta
+    Conta identificadorConta nomeConta cpfConta numeroContaConta dataNascimentoConta enderecoConta senhaConta perguntaSecretaConta respostaSecretaConta saldoConta
 
-somaValor :: People -> Float -> People
+somaValor :: Conta -> Float -> Conta
 somaValor conta valor = do
     let identificadorConta = (show $ identificador conta)
     let nomeConta = (show $ nome conta)
@@ -34,4 +58,13 @@ somaValor conta valor = do
     let perguntaSecretaConta = (show $ perguntaSecreta conta)
     let respostaSecretaConta = (show $ respostaSecreta conta)
     let saldoConta = (show $ saldo conta) + valor
-    People identificadorConta nomeConta cpfConta numeroContaConta dataNascimentoConta enderecoConta senhaConta perguntaSecretaConta respostaSecretaConta saldoConta
+    Conta identificadorConta nomeConta cpfConta numeroContaConta dataNascimentoConta enderecoConta senhaConta perguntaSecretaConta respostaSecretaConta saldoConta
+
+
+salvaConta :: Conta -> IO
+salvaConta = do
+    putStrLn "Conta salva com sucesso"
+
+pegarDiaAtual :: IO UTCTime
+pegarDiaAtual = getCurrentTime >>= return
+    
