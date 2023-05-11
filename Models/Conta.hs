d172fee131ecc01cb1c63a6bef0bd130f7eee3ca
Raniel <rnldourado@gmail.com>
2023-05-11 03:51:52 -0300

    feat: funções de CRUD para I/O no CSV

---
+++ Models/Conta.hs
@@ -0,0 +1,22 @@
+module Models.Conta where
+
+import GHC.Generics    
+import Data.Time.Clock (UTCTime)
+import Data.Time (UTCTime)
+import GHC.Base (Float)
+import Models.Emprestimo as Emprestimo
+
+data Conta = Conta {
+    identificador :: Int,
+    nome :: String,
+    numeroConta :: String,
+    cpf :: String,
+    dataNascimento :: UTCTime,
+    endereco :: String,
+    senha :: String,
+    perguntaSecreta :: String,
+    respostaSecreta :: String,
+    saldo :: Float,
+    acoes :: [Int],
+    emprestimo :: Emprestimo
+} deriving (Show, Generic)
---
+++ Models/Emprestimo.hs
@@ -0,0 +1,11 @@
+module Models.Emprestimo where
+import GHC.Generics
+import Data.Time (UTCTime)
+
+data Emprestimo = Emprestimo {
+    valorTotal :: Float,
+    proximaParcela :: Float,
+    dataProximaParcela :: UTCTime,
+    quantParcelasRestantes :: Int,
+    taxaJuros :: Float
+} deriving (Show, Generic)
