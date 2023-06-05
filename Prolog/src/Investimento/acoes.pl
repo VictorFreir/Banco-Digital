98bdcd5c3bdb8a438ce2c62a05f661426a3b0df4
abraaovalentim <abraaotaku@gmail.com>
2023-06-05 17:48:21 -0300

    finaliza metodo de print e de atualização de valor

--- Prolog/src/Investimento/acoes.pl
+++ Prolog/src/Investimento/acoes.pl
@@ -1,22 +1,22 @@
-:- module(acoes, []).
+:- module(acoes).
+:- dynamic acao/3.
 
-acao(Nome,Preco,Dividendo)
 acao(pixget,10.85,0.01).
 acao(haiscompany,3.76,0.03).
 acao(muquiff,1.49,0.08).
 
-getPreco(Nome, Preco) :-
-    acao(Nome, Preco, _).
-
-novo_preco(Acao, NovoPreco) :-
+atualizaPrecoNaRegra(Acao, Preco):-
     retract(acao(Acao, _, _)),
-    assertz(acao(Acao, NovoPreco, _)).
+    assert(acao(Acao, Preco, _)).
 
 calculaRendimento(ValorAtual, ValorNovo):-
     random(0, 3, Numero),
-    (Numero =:= 0 -> ValorNovo is ValorAtual * 0.90 ; ValorNovo is ValorAtual * 1.10).
+    (Numero =:= 0 -> Valor is ValorAtual * 0.90 ; Valor is ValorAtual * 1.10),
+    ValorNovo is round(Valor * 100) / 100.
 
 #daqui pra frente ta errado, nao pega certo depois eu ajeito
+getPreco(Acao, Preco):-
+    acao(Acao, Preco, _).
 
 atualizaTodos:-
     acao(pixget,PrecoPG,0.01),
@@ -25,15 +25,22 @@ atualizaTodos:-
     calculaRendimento(PrecoPG, NPG),
     calculaRendimento(PrecoHC, PHC),
     calculaRendimento(PrecoMQ, PMQ),
-    novo_preco(pixget, NPG),
-    novo_preco(haiscompany, PHC),
-    novo_preco(muquiff, PMQ).
+    atualizaPrecoNaRegra(pixget, NPG),
+    atualizaPrecoNaRegra(haiscompany, PHC),
+    atualizaPrecoNaRegra(muquiff, PMQ).
 
-printaAcoes:-
-    getPreco(pixget, PrecoPG),
-    getPreco(haiscompany,PrecoHC),
-    getPreco(muquiff,PrecoMQ),
+printAllAcoes:-
+    acao(pixget,PrecoPG,0.01),
+    acao(haiscompany,PrecoHC,0.03),
+    acao(muquiff,PrecoMQ,0.08),
     write('1 - PixGet - Valor: R$ '), write(PrecoPG), write(' - Dividendos: 1%'), nl,
     write('2 - HaisCompany - Valor: R$ '), write(PrecoHC), write(' - Dividendos: 3%'), nl,
     write('3 - Muquiff - Valor: R$ '), write(PrecoMQ), write(' - Dividendos: 8%'), nl.
 
+
+main:-
+    printAllAcoes,
+    atualizaTodos,
+    printAllAcoes,
+    atualizaTodos,
+    printAllAcoes.
