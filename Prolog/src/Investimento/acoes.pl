e023b3d48b9836d681f123a034634da651ea04e3
abraaovalentim <abraaotaku@gmail.com>
2023-06-06 16:49:02 -0300

    finaliza todas as funcionalidades defaults sem acesso as informações da conta do usuario.

--- Prolog/src/Investimento/acoes.pl
+++ Prolog/src/Investimento/acoes.pl
@@ -1,46 +1,89 @@
 :- module(acoes).
-:- dynamic acao/3.
+:- dynamic acao/4.
+:- use_module(library(csv)).
 
-acao(pixget,10.85,0.01).
-acao(haiscompany,3.76,0.03).
-acao(muquiff,1.49,0.08).
-
-atualizaPrecoNaRegra(Acao, Preco):-
-    retract(acao(Acao, _, _)),
-    assert(acao(Acao, Preco, _)).
+atualizaPrecoNaRegra(IdAcao, Preco):-
+    retract(acao(IdAcao,Nome, _,Div)),
+    assert(acao(IdAcao,Nome, Preco, Div)).
 
 calculaRendimento(ValorAtual, ValorNovo):-
     random(0, 3, Numero),
     (Numero =:= 0 -> Valor is ValorAtual * 0.90 ; Valor is ValorAtual * 1.10),
     ValorNovo is round(Valor * 100) / 100.
 
-#daqui pra frente ta errado, nao pega certo depois eu ajeito
-getPreco(Acao, Preco):-
-    acao(Acao, Preco, _).
-
 atualizaTodos:-
-    acao(pixget,PrecoPG,0.01),
-    acao(haiscompany,PrecoHC,0.03),
-    acao(muquiff,PrecoMQ,0.08),
+    acao(1,pixget,PrecoPG,0.01),
+    acao(2,haiscompany,PrecoHC,0.03),
+    acao(3,muquiff,PrecoMQ,0.08),
     calculaRendimento(PrecoPG, NPG),
     calculaRendimento(PrecoHC, PHC),
     calculaRendimento(PrecoMQ, PMQ),
-    atualizaPrecoNaRegra(pixget, NPG),
-    atualizaPrecoNaRegra(haiscompany, PHC),
-    atualizaPrecoNaRegra(muquiff, PMQ).
+    atualizaPrecoNaRegra(1, NPG),
+    atualizaPrecoNaRegra(2, PHC),
+    atualizaPrecoNaRegra(3, PMQ).
 
 printAllAcoes:-
-    acao(pixget,PrecoPG,0.01),
-    acao(haiscompany,PrecoHC,0.03),
-    acao(muquiff,PrecoMQ,0.08),
+    acao(1,pixget,PrecoPG,0.01),
+    acao(2,haiscompany,PrecoHC,0.03),
+    acao(3,muquiff,PrecoMQ,0.08),
     write('1 - PixGet - Valor: R$ '), write(PrecoPG), write(' - Dividendos: 1%'), nl,
     write('2 - HaisCompany - Valor: R$ '), write(PrecoHC), write(' - Dividendos: 3%'), nl,
     write('3 - Muquiff - Valor: R$ '), write(PrecoMQ), write(' - Dividendos: 8%'), nl.
 
+compraAcoes:-
+    write('Digite o ID da acao que deseja comprar:'), nl,
+    printAllAcoes,
+    read(IdAcao),
+    write('Digite a quantidade de acoes que deseja comprar:'), nl,
+    read(Quantidade),
+    acao(IdAcao, _, Preco, _),
+    Valor is Preco * Quantidade,
+    write('Valor total da compra: R$ '), write(Valor), nl,
+    write('Digite 1 para confirmar ou 0 para cancelar:'), nl,
+    read(Confirmacao),
+    (Confirmacao =:= 1 -> 
+        write('Compra realizada com sucesso!'); write('Compra cancelada!')),nl.
 
-main:-
-    printAllAcoes,
-    atualizaTodos,
-    printAllAcoes,
-    atualizaTodos,
-    printAllAcoes.
+vendeAcoes:- 
+    minhasAcoes,
+    write('Digite o ID da acao que deseja vender:'), nl,
+    read(IdAcao),
+    write('Digite a quantidade de acoes que deseja vender:'), nl,
+    read(Quantidade),
+    acao(IdAcao, _, Preco, _), 
+    Valor is Preco * Quantidade,
+    write('Valor total da venda: R$ '), write(Valor), nl,
+    write('Digite 1 para confirmar ou 0 para cancelar:'), nl,
+    read(Confirmacao),
+    (Confirmacao =:= 1 -> 
+        write('Venda realizada com sucesso!'),nl,
+        write('Voce recebeu R$ '), write(Valor), write(' em sua conta!')
+        ; write('Venda cancelada!')),nl.
+
+minhasAcoes:-
+    write('Atualmente, voce possui as seguintes acoes:'), nl,
+    write('1 - PixGet - Quantidade: 100'), nl,
+    write('2 - HaisCompany - Quantidade: 100'), nl,
+    write('3 - Muquiff - Quantidade: 100'), nl.
+
+resgatarDividendos:-
+    write('Digite o ID da acao que deseja resgatar os dividendos:'), nl,
+    read(IdAcao),
+    acao(IdAcao, _, Preco, Dividendos),
+    Valor is Preco * Dividendos,
+    write('Valor total dos dividendos: R$ '), write(Valor), nl,
+    write('Digite 1 para confirmar ou 0 para cancelar:'), nl,
+    read(Confirmacao),
+    (Confirmacao =:= 1 -> 
+        write('Dividendos resgatados com sucesso!'), nl,
+        write('Voce recebeu R$ '), write(Valor), write(' em sua conta!')
+        ; write('Resgate cancelado!')).
+
+
+readFromCsvToAcao:-
+    csv_read_file('acoes.csv', Rows, [functor(acao), arity(4)]),
+    maplist(assert, Rows).
+
+formatFromAcaoToCsv:-
+    findall(acao(IdAcao,Nome,Preco,Div), acao(IdAcao,Nome,Preco,Div), Acoes),
+    csv_write_file('acoes.csv', Acoes).
--- Prolog/src/Investimento/menuAcoes.pl
+++ Prolog/src/Investimento/menuAcoes.pl
@@ -1,4 +1,5 @@
-:- module(menuAcoes, []).
+:- module(menuAcoes).
+:- consult('acoes.pl').
 
 exibeFuncionalidadesAcoes:-
     write('Selecione a acao desejada com apenas o numero referente:'), nl,
@@ -8,4 +9,49 @@ exibeFuncionalidadesAcoes:-
     write('  4- Ver Minhas Acoes'), nl,
     write('  5- Resgatar Dividendos'), nl,
     write('  6- Sair'), nl.
+
+selecionaFuncionalidadeAcoes(1):-
+    shell(clear),
+    printAllAcoes,
+    exibeFuncionalidadesAcoes,
+    read(Entrada),
+    atualizaTodos,
+    selecionaFuncionalidadeAcoes(Entrada).
+selecionaFuncionalidadeAcoes(2):-
+    shell(clear),
+    compraAcoes,
+    exibeFuncionalidadesAcoes,
+    read(Entrada),
+    atualizaTodos,
+    selecionaFuncionalidadeAcoes(Entrada).
+selecionaFuncionalidadeAcoes(3):-
+    shell(clear),ém os dados necessários para a execução correta do código.
+
+
+    atualizaTodos,
+    selecionaFuncionalidadeAcoes(Entrada).
+selecionaFuncionalidadeAcoes(4):-
+    shell(clear),
+    minhasAcoes,
+    exibeFuncionalidadesAcoes,
+    read(Entrada),
+    atualizaTodos,
+    selecionaFuncionalidadeAcoes(Entrada).
+selecionaFuncionalidadeAcoes(5):-
+    shell(clear),
+    resgatarDividendos,
+    exibeFuncionalidadesAcoes,
+    read(Entrada),
+    atualizaTodos,
+    selecionaFuncionalidadeAcoes(Entrada).
+selecionaFuncionalidadeAcoes(6):-
+    shell(clear),
+    formatFromAcaoToCsv,
+    write('Saindo...'), nl.
+main:-
+    readFromCsvToAcao,
+    shell(clear),
+    exibeFuncionalidadesAcoes,
+    read(Entrada),
+    selecionaFuncionalidadeAcoes(Entrada).
     
