8d047a14cfc1fe7431e1c9ce2cbb51d2ede8cb5d
Raniel <rnldourado@gmail.com>
2023-06-07 01:35:04 -0300

    doc: bug

--- Prolog/src/database.pl
+++ Prolog/src/database.pl
@@ -49,3 +49,11 @@ deletaConta(CPF) :-
 registrarDadosNoCSV :-
     findall(conta(ID, Nome, NumeroConta, CPF, DataNascimento, Endereco, Senha, PerguntaSecreta, RespostaSecreta, Saldo, Acao1, Acao2, Acao3, ValorTotal, ProximaParcela, DataProximaParcela, QtdParcelasRestantes, TaxaJuros), conta(ID, Nome, NumeroConta, CPF, DataNascimento, Endereco, Senha, PerguntaSecreta, RespostaSecreta, Saldo, Acao1, Acao2, Acao3, ValorTotal, ProximaParcela, DataProximaParcela, QtdParcelasRestantes, TaxaJuros), Contas),
     csv_write_file('./Prolog/resources/db.csv', Contas).
+
+% Está com um bug ao alterar, ler e escrever no CSV
+% O bug ele não altera e sim cria uma nova conta apenas com o saldo alterado e os demais nulos ([]).
+% 
+% Exemplo do CSV:
+%
+% 4,Marcelo,55789-964,10,15/06/1987,Rua dos bobos Nº 0,1234,Nome do gato,Saruê,950.0,0,0,0,0.0,200.0,07/2023,13,0.15
+% [],[],[],10,[],[],[],[],[],8000,[],[],[],[],[],[],[],[]
