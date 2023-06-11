37ca7b23abed58225e750728fb2c225b89a54a2b
VictorFreir <victorvfreire@gmail.com>
2023-06-10 21:19:18 -0300

    Menu Funcionalidades

---
+++ Prolog/src/menuFuncionalidades.pl
@@ -0,0 +1,52 @@
+:- initialization(main).
+:- initialization(quiet).
+
+:- include('transferencia.pl').
+:- include('consultarEmprestimo.pl').
+:- include('solicitarEmprestimo.pl').
+:- discontiguous buscaSaldo/2.
+:- discontiguous subtraiSaldo/3.
+:- discontiguous sair/0.
+:- discontiguous conta/3.
+:- discontiguous calculaValorTotalEmprestimo/2.
+
+menuFuncionalidades:-
+    write("=============== menu principal ==============="),nl,
+    write("Digite a operacao desejada apenas com o numero:"),nl,
+    write("1- Consultar Saldo"),nl,
+    write("2- Transferir"),nl,
+    write("3- Sacar"),nl,
+    write("4- Consultar extrato"),nl,
+    write("5- Poupanca"),nl,
+    write("6- Emprestimos"),nl,
+    write("7- Acoes"),nl,
+    write("8- Sair"),nl,
+    read(Opcao),
+    Cpf is 15241593446,
+    seletorFuncionalidades(Opcao,Cpf),
+    menuFuncionalidades.
+
+seletorFuncionalidades(1,Cpf):-
+    true.
+
+seletorFuncionalidades(2,Cpf):-
+    menuTransferencia(Cpf).
+
+seletorFuncionalidades(3,Cpf):-
+    true.
+
+seletorFuncionalidades(4,Cpf):-
+    true.
+
+seletorFuncionalidades(5,Cpf):-
+    true.
+
+seletorFuncionalidades(6,Cpf):-
+    true.
+
+seletorFuncionalidades(7,Cpf):-
+    true.
+
+seletorFuncionalidades(_,_):-
+    write("=============== Fim da execucao ==============="),
+    halt.
